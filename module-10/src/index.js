0fb05f800bcd933d178a591f7c7d98255a2abb35
Anastasia-Dmv <Vesta50000@gmail.com>
2020-07-10 09:32:01 +0300

    10 mod modal work

--- module-10/src/index.js
+++ module-10/src/index.js
@@ -1,5 +1,48 @@
 import './styles.css';
  import  './js/menu';
 
-//  import '.js/templating';
+
  import './js/localstorage';
+ import './js/cart';
+
+ import * as basicLightbox from 'basiclightbox';
+ import 'basiclightbox/dist/basicLightbox.min.css';
+
+
+ const openModalBtnRef = document.querySelector('.js-button');
+ const modalTemplate = document.querySelector('#modal');
+
+ const instance = basicLightbox.create(modalTemplate, {
+     onShow(instance)  {
+         const closeModalBtnRef = getCloseModalBtnRef(instance);
+         closeModalBtnRef.addEventListener('click', instance.close);
+     },
+     onClose(instance){
+         const closeModalBtnRef = getCloseModalBtnRef(instance);
+         closeModalBtnRef.removeEventListener('click', instance.close);
+     },
+ });
+ openModalBtnRef.addEventListener('click', instance.show);
+
+ function getCloseModalBtnRef(parent){
+ return parent.element().querySelector('button[data-close-modal]');
+};
+
+
+
+
+
+ //========================
+
+//  const openModalBtnRef = document.querySelector('.js-button');
+//  const modalTemplate = document.querySelector('#modal');
+
+//  const instance = basicLightbox.create(modalTemplate, {
+//      onShow: instance => {
+//          const closeModalBtnRef = instance
+//          .element()
+//          .querySelector('button[data-close-modal]');
+//          closeModalBtnRef.onclick = instance.close;
+//      },
+//  });
+//  openModalBtnRef.addEventListener('click', instance.show);
