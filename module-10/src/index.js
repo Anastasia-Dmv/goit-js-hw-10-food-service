20ebbd57ab35f64c1ebc82c667e0dbd609bd0a40
Anastasia-Dmv <Vesta50000@gmail.com>
2020-07-12 12:40:42 +0300

    10mod food cart

--- module-10/src/index.js
+++ module-10/src/index.js
@@ -1,48 +1,45 @@
 import './styles.css';
- import  './js/menu';
+import './cart/cart';
+import  './js/menu';
+import './js/localstorage';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
-
- import './js/localstorage';
- import './js/cart';
-
- import * as basicLightbox from 'basiclightbox';
- import 'basiclightbox/dist/basicLightbox.min.css';
-
-
- const openModalBtnRef = document.querySelector('.js-button');
- const modalTemplate = document.querySelector('#modal');
-
- const instance = basicLightbox.create(modalTemplate, {
-     onShow(instance)  {
-         const closeModalBtnRef = getCloseModalBtnRef(instance);
-         closeModalBtnRef.addEventListener('click', instance.close);
-     },
-     onClose(instance){
-         const closeModalBtnRef = getCloseModalBtnRef(instance);
-         closeModalBtnRef.removeEventListener('click', instance.close);
-     },
- });
- openModalBtnRef.addEventListener('click', instance.show);
-
- function getCloseModalBtnRef(parent){
- return parent.element().querySelector('button[data-close-modal]');
-};
+//===========
+export const bodyRef = document.querySelector('body');
+export  const switchRef = document.querySelector('.js-switch-input');
 
 
 
+ //============
+ 
 
 
- //========================
 
 //  const openModalBtnRef = document.querySelector('.js-button');
 //  const modalTemplate = document.querySelector('#modal');
 
 //  const instance = basicLightbox.create(modalTemplate, {
-//      onShow: instance => {
-//          const closeModalBtnRef = instance
-//          .element()
-//          .querySelector('button[data-close-modal]');
-//          closeModalBtnRef.onclick = instance.close;
+//      onShow(instance)  {
+//          const closeModalBtnRef = getCloseModalBtnRef(instance);
+//          closeModalBtnRef.addEventListener('click', instance.close);
+//      },
+//      onClose(instance){
+//          const closeModalBtnRef = getCloseModalBtnRef(instance);
+//          closeModalBtnRef.removeEventListener('click', instance.close);
 //      },
 //  });
 //  openModalBtnRef.addEventListener('click', instance.show);
+
+//  function getCloseModalBtnRef(parent){
+//  return parent.element().querySelector('button[data-close-modal]');
+// };
+
+
+
+
+
+
+
+
+
